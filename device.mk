b07428377ae948bfce2ee18403d515bda1a20202
romgharti <romgharti@gmail.com>
2022-02-22 11:58:32 +0545

    disable xiaomi parts

--- device.mk
+++ device.mk
@@ -154,7 +154,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # Device-specific settings
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     XiaomiParts
 
 # Display
