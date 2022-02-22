7a56aabf751c8bd02d72d637938b10fdfe07a55b
romgharti <romgharti@gmail.com>
2022-02-22 22:55:24 +0545

    mojito: vintf: Import power.xml from stock
    - Fix vintf checking
    - Error:
    checkvintf E 02-22 14:31:49 3726 3726 check_vintf.cpp:620] files are incompatible: Device manifest and framework compatibility matrix are incompatible: HALs incompatible. Matrix level = 5. Manifest level = 5. The following requirements are not met:
    
    checkvintf E 02-22 14:31:49 3726 3726 check_vintf.cpp:620] android.hardware.power:
    
    checkvintf E 02-22 14:31:49 3726 3726 check_vintf.cpp:620] required: IPower/default (@1-2)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,7 @@ BOARD_HAVE_QCOM_FM := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/power.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Filesystem
