a5eb7e3fbd52566ca92690dfffeb103677a1ce63
Bowgo Tsai <bowgotsai@google.com>
2022-04-20 13:56:23 +0545

    mojito: Adding developer GSI keys Bug: 144399552 Test: tree-hugger Change-Id: I0cf78bfc80c24bb217bb88831faa5a29ba04d194

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
