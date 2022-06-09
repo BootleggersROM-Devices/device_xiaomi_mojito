632d6e0a007d8b13d10849ce9320d2a96b57fc20
Mathieu Chartier <mathieuc@google.com>
2022-06-09 18:49:01 +0545

    mojito: Speed profile services and wifi-service to reduce RAM … …and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- device.mk
+++ device.mk
@@ -423,6 +423,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/qcom-caf/bootctrl
 
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
