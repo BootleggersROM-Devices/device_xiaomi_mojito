9195d567c4b868bd543da8c17bc41d7f586d9810
xSylla <syllamodder@gmail.com>
2022-07-26 19:32:25 +0545

    mojito: Enforce fstab suffix to qcom Change-Id: I5b7246196ec57020cdf980a4867b6b9b08ab3087

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_USE_LZ4 := true
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
