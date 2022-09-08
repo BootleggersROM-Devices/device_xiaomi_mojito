ac760574236d99e37184743cbb50e83e4708fb3f
boedhack99 <boedhack@gmail.com>
2022-09-08 17:53:45 +0545

    mojito: Add soundtrigger@2.0 to manifest * E init    : Control message: Could not find 'android.hardware.soundtrigger@2.0::ISoundTriggerHw/default' for ctl.interface_start from pid: 478 (/system/bin/hwservicemanager)

--- device.mk
+++ device.mk
@@ -50,6 +50,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
+    android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
