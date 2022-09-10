31a69d992997e51e2cfbd55e632e8753ca80d6f2
RONIN808 <shubhamshedage808@gmail.com>
2022-09-10 09:01:36 +0545

    mojito: Redefine Surfaceflinger Properties as per CAF

--- device.mk
+++ device.mk
@@ -368,6 +368,12 @@ PRODUCT_PACKAGES += \
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_virtual_display_dimension=4096
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
