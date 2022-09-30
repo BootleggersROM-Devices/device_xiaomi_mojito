8dd6c2849ec440236edd916bee6edec3cf56f245
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-09-30 10:02:50 +0700

    [SQUASH] mojito: init: Rewrite for fix play protect issue
    Revert "mojito: Add custom init to set proper variants"
    
    This reverts commit 27ae7b0e23f48e0d18232a1aadf6fda1c8e15aa0.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    mojito: Add custom init to override properties
    
    * Use latest sunny properties.
    * Kang from: https://github.com/AOSPA/android_device_xiaomi_alioth/blob/topaz/init/init_alioth.cpp
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I0335da461539021e5c63aa95bccb974cdd7aaa3f
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    mojito: init: Add mojito variant support
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    mojito: init: Setup dalvikvm properties
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    mojito: init: Split common properties into one function
    
    Common properties should only be defined once.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    mojito: init: Fix wrong model props per variant
    
    * sunny variant using M2101K7AG
    * mojito variant using M2101K7AI
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    mojito: init: Merge vendor properties into one function
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    mojito: init: Commonize override dalvikvm heapsize and heapminfree props
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    mojito: init: Set model name props to `Redmi Note 10`
    
    The current target ROM is not MIUI based, and setting the
    model name property to either M2101K7AG or M2101K7AI causes
    an error detecting the device model on the Play Store.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    Co-authored-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Co-authored-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- libinit/init_mojito.cpp
+++ libinit/init_mojito.cpp
@@ -1,115 +1,85 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2016 The CyanogenMod Project.
-   Copyright (C) 2019-2020 The LineageOS Project.
-   Copyright (C) 2021 Paranoid Android.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2022 PixelExperience
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include <stdlib.h>
+#include <vector>
+
+#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
 
-#include <android-base/properties.h>
-#include "property_service.h"
-#include "vendor_init.h"
-
 using android::base::GetProperty;
-using std::string;
 
-void property_override(string prop, string value)
-{
-    auto pi = (prop_info*) __system_property_find(prop.c_str());
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info* pi;
 
-    if (pi != nullptr)
-        __system_property_update(pi, value.c_str(), value.size());
-    else
-        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+    pi = (prop_info*)__system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void vendor_load_properties()
-{
-    string device, model;
-
-    string hwname = GetProperty("ro.boot.hwname", "");
-
-    if (hwname == "sunny") {
-        device = "sunny";
-        model = "Redmi Note 10";
-    } else {
-        device = "mojito";
-        model = "Redmi Note 10";
-    }
-
-    // Override all partitions' props
-    string prop_partitions[] = { "", "bootimage.", "odm.", "product.", "system.", "system_ext.", "vendor." };
-    for (const string &prop : prop_partitions) {
-        property_override(string("ro.product.") + prop + string("board"), device);
-        property_override(string("ro.product.") + prop + string("device"), device);
-        property_override(string("ro.product.") + prop + string("name"), device);
-        property_override(string("ro.product.") + prop + string("model"), model);
-        property_override(string("ro.") + prop + string("build.product"), device);
-    }
-
-    // Set hardware revision
-    property_override("ro.boot.hardware.revision", GetProperty("ro.boot.hwversion", "").c_str());
-
-    // Set hardware SKU prop
-    property_override("ro.boot.product.hardware.sku", device);
-
-    // Set dalvik heap configuration
-    string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
-			heapmaxfree, heaptargetutilization;
-
+void load_dalvikvm_props() {
     struct sysinfo sys;
     sysinfo(&sys);
 
     if (sys.totalram > 5072ull * 1024 * 1024) {
         // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heapgrowthlimit", "256m");
+        property_override("dalvik.vm.heaptargetutilization", "0.5");
+        property_override("dalvik.vm.heapmaxfree", "32m");
     } else if (sys.totalram > 3072ull * 1024 * 1024) {
         // from - phone-xhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
+        property_override("dalvik.vm.heapstartsize", "8m");
+        property_override("dalvik.vm.heapgrowthlimit", "192m");
+        property_override("dalvik.vm.heaptargetutilization", "0.6");
+        property_override("dalvik.vm.heapmaxfree", "16m");
     }
 
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override("dalvik.vm.heapsize", "512m");
+    property_override("dalvik.vm.heapminfree", "8m");
+}
+
+void load_vendor_props() {
+    std::string hwname = GetProperty("ro.boot.hwname", "");
+    if (hwname.find("sunny") != std::string::npos) {
+        property_override("ro.product.device", "sunny");
+        property_override("ro.product.name", "sunny");
+        property_override("ro.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V13.0.10.0.SKGMIXM:user/release-keys");
+        property_override("ro.vendor.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V13.0.10.0.SKGMIXM:user/release-keys");
+        property_override("ro.bootimage.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V13.0.10.0.SKGMIXM:user/release-keys");
+        property_override("ro.build.description", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V13.0.10.0.SKGMIXM:user/release-keys");
+    } else {
+        property_override("ro.product.device", "mojito");
+        property_override("ro.product.name", "mojito");
+        property_override("ro.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V13.0.10.0.SKGMIXM:user/release-keys");
+        property_override("ro.vendor.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V13.0.10.0.SKGMIXM:user/release-keys");
+        property_override("ro.bootimage.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V13.0.10.0.SKGMIXM:user/release-keys");
+        property_override("ro.build.description", "Redmi/mojito/mojito:12/RKQ1.210614.002/V13.0.10.0.SKGMIXM:user/release-keys");
+    }
+
+    property_override("bluetooth.device.default_name", "Redmi Note 10");
+    property_override("ro.product.brand", "Redmi");
+    property_override("ro.product.manufacturer", "Xiaomi");
+    property_override("ro.product.model", "Redmi Note 10");
+}
+
+void vendor_load_properties() {
+    load_vendor_props();
+    load_dalvikvm_props();
 }
