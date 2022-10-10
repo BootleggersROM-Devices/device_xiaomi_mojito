9fc41d141333ebfed535e942ef3bb11455618e34
Zinadin Zidan <zidan44@pixelexperience.org>
2022-10-10 23:45:37 +0600

    mojito: camera: Import camxoverridesettings txt
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I34a9fc370bab422d3a340f72e37a51593572fc71
    Signed-off-by: Fiqri Ardyansyah <fiqri19102022@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
