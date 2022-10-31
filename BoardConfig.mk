66469987a127f3a97916217ca1b09dfcf804a373
romgharti <romgharti@gmail.com>
2022-10-31 11:20:53 +0545

    Revert "mojito: Remove Power HAL interaction boost"
    This reverts commit 57fc2a7faf9f9c32594a40e039a83f4ace47808e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,6 +168,7 @@ TARGET_BOARD_PLATFORM := sm6150
 
 # Power
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
