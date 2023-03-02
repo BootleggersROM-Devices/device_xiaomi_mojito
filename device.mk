669b2f0368bfa0a7cc3a28ea50f8587144eebd09
Manjunatha Madana <quic_c_mamanj@quicinc.com>
2023-03-02 22:06:18 +0545

    mojito: Disable surfaceflinger prime shader cache Set property service.sf.prime_shader_cache=0 to remove the cache and save memory in the surfaceflinger process.
    Change-Id: I76054bbe68594b8998082580813e80136311a6ff
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -402,7 +402,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.max_virtual_display_dimension=4096
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    service.sf.prime_shader_cache=0
 
 # QTI
 PRODUCT_PACKAGES += \
