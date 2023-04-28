bd5eebcb5c901ed719a6199509229e3dda0af782
Rama Bondan Prakoso <ramanarubp@gmail.com>
2023-04-28 14:11:00 +0545

    mojito: Droped TARGET_USES_INTERACTION_BOOST * Make cpu frequency more agressive, when user touching the screen   Frequency always jump to max frequency then lead more battery cost. * IMO this flag only designed for device that have much frequency step.   So this is useless for our device since just have few frequency step.
    Signed-off-by: romgharti <romgharti@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,7 +186,6 @@ TARGET_BOARD_PLATFORM := sm6150
 # Power
 TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/power/boost-ext.cpp
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
