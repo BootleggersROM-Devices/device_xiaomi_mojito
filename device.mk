e2167ec3badc02d27fa253a5a2d9650612b124a6
romgharti <romgharti@gmail.com>
2023-08-08 19:13:24 +0545

    mojito: Build sensorservice 1.0 HAL -W/HidlServiceManagement(  759): getService: unable to call into hwbinder service for android.frameworks.sensorservice@1.0::ISensorManager/default.
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -444,6 +444,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
