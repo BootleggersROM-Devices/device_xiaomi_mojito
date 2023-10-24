5adc00773e4a33207a61dd99d435497c58da8718
nishant6342 <www.rajsonu13@gmail.com>
2023-10-24 22:03:23 +0600

    mojito: Allow updatable apex again * Needed on Android U
    Signed-off-by: SwapnilVicky <swapnil.motoe@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, vendor/xiaomi/mojito-miuicamera/products/miuicamera.mk)
 
+# Apex
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
