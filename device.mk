dcba8e893192d999ffdbce254b9bb8ecacd312cb
romgharti <romgharti@gmail.com>
2023-10-30 00:34:38 +0545

    Revert "mojito: Add QTI health implementation"
    This reverts commit 658e4ac19df5fdf8f751426a21a60e8fbc37f9ea.

--- device.mk
+++ device.mk
@@ -254,7 +254,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
     android.hardware.health@2.1-impl.recovery
 
