88c327ba0d2bb028dd240e63d03daefa78a0fddf
romgharti <romgharti@gmail.com>
2023-12-17 06:07:44 +0545

    Revert "mojito: Move to QTI health AIDL service"
    This reverts commit 0bb9edf2707ba44c75ee12ca08cd6f58f5836dcb.

--- device.mk
+++ device.mk
@@ -246,8 +246,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
