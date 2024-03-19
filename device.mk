2a79a1090c056bcb34bf647e399a733714869df6
romgharti <romgharti@gmail.com>
2024-03-19 20:10:28 +0545

    mojito: Drop Surfaceflinger Properties *TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS *NUM_FRAMEBUFFER_SURFACE_BUFFERS *MAX_VIRTUAL_DISPLAY_DIMENSION
    *Fixes system UI lags while PIP.
    
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -412,12 +412,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.max_virtual_display_dimension=4096
-
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
